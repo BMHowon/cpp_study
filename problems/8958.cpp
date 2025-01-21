b7aa297d35085a92ca9c6278e4d3c5df02883202
Howon <cheerhow@naver.com>
2025-01-22 01:16:23 +0900

    Day 52 Commit

--- problems/8958.cpp
+++ problems/8958.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
 using namespace std;
 
 int main() {
 
-    
+    int n;
+    cin >> n;
+    cin.ignore();
+
+    for(int i = 0; i < n; i++) {
+        string result;
+        getline(cin, result);
+
+        int score = 0;
+        int straight = 0;
+
+        for(char ch : result) {
+            if(ch == 'O') {
+                straight++;
+                score += straight;
+            } else {
+                straight = 0;
+            }
+        }
+        cout << score << "\n";
+    }
 
     return 0;
 }
